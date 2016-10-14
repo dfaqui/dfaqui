d526b79695595740a784f6819b6d20a6d26a84d3
Filipe Menezes <filipepmenezes@gmail.com>
2016-10-14 10:38:36 -0300

    Correção na importação do CSS de acordo com o canal

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( core.js base.css base.js admin.scss admin.js )
-Rails.application.config.assets.precompile += %w( application/delivery.scss application/imoveis.scss )
+Rails.application.config.assets.precompile += %w( application/comercio.scss application/imoveis.scss )
