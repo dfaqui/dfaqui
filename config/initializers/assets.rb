2a85a0ed240dcb4226cb884141db42c6bb95337b
Filipe Menezes <filipepmenezes@gmail.com>
2016-11-22 01:34:06 -0200

    Páginas administrativas parcialmente migradas para o novo formato de assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
+  admin.js
   core.js
   region-filter.js.coffee
   market-filter.js.coffee
